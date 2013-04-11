87765f74ea4231a8fa543f91d2cd27c3ce203ece
iwillwen <willwengunn@gmail.com>
2013-04-11 17:06:31 +0800

    Update Redis API and README

--- nanodb.js
+++ nanodb.js
@@ -72,6 +72,14 @@
       return target;
     }
   };
+  if (Object.create === undefined) {
+    Object.create = function(o) {
+      function F() {}
+      F.prototype = o;
+      return new F();
+    };
+  }
+
   if ('undefined' !== typeof window && 'undefined' !== typeof document) {
     var addEvent = (function(){if(document.addEventListener){return function(el,type,fn){if(el&&el.nodeName||el===window){el.addEventListener(type,fn,false)}else if(el&&el.length){for(var i=0;i<el.length;i++){addEvent(el[i],type,fn)}}}}else{return function(el,type,fn){if(el&&el.nodeName||el===window){el.attachEvent('on'+type,function(){return fn.call(el,window.event)})}else if(el&&el.length){for(var i=0;i<el.length;i++){addEvent(el[i],type,fn)}}}}})();
   }
